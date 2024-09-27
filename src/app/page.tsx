2aebf4774e2c14b589b24c4c5bb15b7dd4abab14
Keshav Chamria <keshavchamria@gmail.com>
2024-09-27 23:01:28 +0530

    fetching through apollo client

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,60 +1,41 @@
 "use client";
 import { useEffect, useState } from "react";
-import { queries } from "./constants";
+import { gql, useQuery } from "@apollo/client";
+import client from "../lib/apolloClient"; //
 import ResultTable from "./ResultTable";
+import { queries } from "./constants";
 import styles from "./page.module.scss";
 
 export default function Home() {
   const [columnsDataList, setColumnsDataList] = useState<string[]>([]);
   const [rowsDataList, setRowsDataList] = useState<Array<Object>>([]);
-  const [loading, setLoading] = useState<boolean>(true);
 
-  const [queryDetails] = useState<{ queryName: string; query: string }>(queries[2]); // change the address according to you query type mention in constant file
+  const [queryDetails] = useState<{ queryName: string; query: string }>(queries[2]);
 
-  const fetchGraphQLData = async () => {
-    const url = "https://api.studio.thegraph.com/query/69140/iitb-demo/version/latest";
+  const GET_DATA_QUERY = gql`
+    ${queryDetails.query}
+  `;
 
-    try {
-      setLoading(true);
-      const response = await fetch(url, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          query: queryDetails.query,
-        }),
-      });
+  const { loading, error, data } = useQuery(GET_DATA_QUERY, { client });
 
-      const data = await response.json();
-
-      if (data.errors) {
-        console.error("GraphQL query error:", data.errors);
-        return;
-      }
-
-      console.log("GraphQL query result:", data.data);
-
-      const extractedColumns = Object.keys(data.data?.[queryDetails.queryName]?.[0] || {});
-      const extractedRows = data.data?.[queryDetails.queryName] || [];
+  useEffect(() => {
+    if (data) {
+      const extractedColumns = Object.keys(data[queryDetails.queryName]?.[0] || {});
+      const extractedRows = data[queryDetails.queryName] || [];
 
       setColumnsDataList(extractedColumns);
       setRowsDataList(extractedRows);
-      setLoading(false);
-    } catch (error) {
-      setLoading(false);
-      console.error("Error fetching GraphQL data:", error);
     }
-  };
 
-  useEffect(() => {
-    fetchGraphQLData();
-  }, []);
+    if (error) {
+      console.error("GraphQL query error:", error);
+    }
+  }, [data, error, queryDetails.queryName]);
 
   return (
     <main className={styles.mainContainer}>
       {loading ? (
-        <p className="text-xl  text-slate-200 text-center font-bold">Fetching...</p>
+        <p className="text-xl text-slate-200 text-center font-bold">Fetching...</p>
       ) : (
         <>
           <h1 className="text-xl text-slate-200 font-bold py-2 px-[15px]">{queryDetails.queryName}</h1>
